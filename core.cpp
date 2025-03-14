6b952270e852f38ddef6c7a4b36e12953446e063
NafiseKian <nafiseyousefikian@gmail.com>
2025-03-14 13:57:08 +0200

    key added

--- core.cpp
+++ core.cpp
@@ -5,7 +5,7 @@
 
 int main() {
 
-    GPTService gptService(apiKey);
+    GPTService gptService("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");
     std::string prompt = "Tell me a joke!";
     std::string response = gptService.getGPTResponse(prompt);
     std::cout << "GPT Response: " << response << std::endl;
