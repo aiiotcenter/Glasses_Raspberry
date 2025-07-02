205002edf07d3c5301d6ac39418d49331dc5aa9c
FarahArya <farhane.arya@gmail.com>
2025-07-02 11:31:08 +0300

    url for testing

--- httpService/httpService.cpp
+++ httpService/httpService.cpp
@@ -13,7 +13,7 @@ public:
     HttpService()
     {
         std::cout << "Constructing HTTP service...\n";
-        serverUrl = "http://livebees.aiiot.center/process_frame";
+        serverUrl = "http://192.168.254.245:5000/process_frame";
         curl_global_init(CURL_GLOBAL_ALL);
     }
 
